b9a47eb0d7db7e91c39df375a6ea4e504fd85b05
Fynn <fynnfluegge@gmx.de>
2016-10-22 00:57:18 +0200

    adding math

---
+++ oreon.engine/oreon.engine/Matrix4f.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "Vec3f.h"
+#include "Quaternion.h"
+
+class Matrix4f {
+
+public:
+	Matrix4f();
+	Matrix4f Identity();
+	Matrix4f Orthographic();
+	Matrix4f Translation(const Vec3f &translation);
+	Matrix4f Rotation(const Vec3f &rotation);
+	Matrix4f Scaling(const Vec3f &scaling);
+	Matrix4f Projection(float fovY, float width, float height, float zNear, float zFar);
+	Matrix4f View(Vec3f &forward, Vec3f &up);
+	Matrix4f mul(const Matrix4f &r);
+	Quaternion mul(const Quaternion &v);
+	float get(int x, int y) const;
+	void set(int x, int y, float value);
+	void toString() const;
+
+private:
+	float m[4][4];
+};
